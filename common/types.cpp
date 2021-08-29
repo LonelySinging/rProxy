73a2b7501aeb2e696ea9dd273be692d00f8b5dc3
星途 <2102084678@qq.com>
2021-08-29 10:53:56 +0800

    添加登录协议

--- common/types.cpp
+++ common/types.cpp
@@ -14,10 +14,21 @@ namespace CMD {
 		data = (cmd_dis_connect*)malloc(sizeof(cmd_dis_connect));
 		
 		if (data) {
-			data->_raw_sid = 0;
-			data->_type = CMD_END_SESSION;
 			data->_sid = sid;
 		}
 		return (char*)data;
 	}
+
+    char* MAKE_cmd_login(us16 passwd_size, char* passwd, us16 des_size, char* des) {
+        cmd_login* data = NULL;
+        data = (cmd_login*)malloc(sizeof(cmd_login));
+        if(data){
+            assert(passwd_size <= sizeof(data->_passwd));
+            assert(des_size <= sizeof(data->_describe));
+            
+            memcpy(data->_passwd, passwd, sizeof(data->_passwd));
+            memcpy(data->_describe, des, sizeof(data->_describe));
+        }
+        return (char*)data;
+    }
 };
--- common/types.h
+++ common/types.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 typedef unsigned short int us16;
 
@@ -25,12 +26,24 @@ namespace CMD {	// 控制指令的sid统一为0
 
 	typedef struct {
         us16 _raw_sid = 0;
-		us16 _type;	// 上面的枚举
+		us16 _type = CMD_END_SESSION;	// 上面的枚举
 		us16 _sid;
 	}cmd_dis_connect;
 
+    typedef struct {
+        enum {
+            PASSWD_LEN = 64,
+            DES_LEN = 128
+        };
+        us16 _raw_sid = 0;
+        us16 _type = CMD_LOGIN;
+        char _passwd[PASSWD_LEN];   // 口令
+        char _describe[DES_LEN];   // 描述
+    }cmd_login;
+
 	// 构造命令数据包
 	char* MAKE_cmd_dis_connect(int sid);
+    char* MAKE_cmd_login(us16 pass_size, char* passwd,us16 des_size, char* des);
 };
 
 
